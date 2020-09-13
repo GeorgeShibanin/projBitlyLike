6917edafbfb4de26e01c3c9fb076e0688aed702f
George Shibanin <grandgoha@gmail.com>
2020-09-14 00:53:56 +0300

    version 0.2

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,4 @@ from wtforms.validators import DataRequired
 class emptyBlank(FlaskForm):
     url = StringField('Your url', validators=[DataRequired()])
     submit = SubmitField('Make it shorter')
+    custom = StringField('Cusomize url')
--- app/routes.py
+++ app/routes.py
@@ -7,23 +7,30 @@ import requests
 import random
 import string
 
+my = "http://georgeshibanin.pythonanywhere.com/"
+
 
 @app.route('/', methods=['GET', 'POST'])
 def changeurl():
     form = emptyBlank()
     if form.validate_on_submit():
         url = request.form['url']
+        cust = request.form['custom']
         if requests.get(url).status_code != 200:
             return "your link is not valid"
         a = Href.query.filter_by(url=url).first()
+
         if not a:
-            newurl = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for x in range(8))
+            if cust != '':
+                newurl = cust
+            else:
+                newurl = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for x in range(8))
             href = Href(url, newurl)
             db.session.add(href)
             db.session.commit()
-            return newurl
+            return my + newurl
         else:
-            return db.session.query(Href.new_url).filter(Href.url == url).scalar()
+            return my + db.session.query(Href.new_url).filter(Href.url == url).scalar()
     return render_template('main.html', title='Change URL', form=form)
 
 
