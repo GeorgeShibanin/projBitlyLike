27745372561d2cff32dda42b70c58268b0d4c7e9
George Shibanin <grandgoha@gmail.com>
2020-09-14 20:47:16 +0300

    version 0.4

---
+++ app/functions.py
@@ -0,0 +1,30 @@
+from flask import request
+from app.models import Href
+from app import db
+import random
+import string
+import requests
+
+
+def short_save(url, cust):
+    if requests.get(url).status_code != 200:
+        return "your link is not valid"
+    a = Href.query.filter_by(url=url).first()
+    if not a:
+        if cust != '':
+            newurl = cust
+        else:
+            newurl = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for x in range(8))
+        href = Href(url, newurl)
+        db.session.add(href)
+        db.session.commit()
+        return newurl
+    else:
+        return db.session.query(Href.new_url).filter(Href.url == url).scalar()
+
+
+def return_url(url):
+    res = db.session.query(Href.url).filter(Href.new_url == url)
+    if not res.first():
+        return "No such url"
+    return res.scalar()
--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,17 @@
 from flask import render_template, request, redirect
 from app.forms import emptyBlank
-from app import app, db
-from app.models import Href
-
-import requests
-import random
-import string
-
-my = "http://georgeshibanin.pythonanywhere.com/"
+from app import app
+from app.functions import short_save, return_url
 
 
 @app.route('/', methods=['GET', 'POST'])
-def changeurl():
+def change_url():
     form = emptyBlank()
     if form.validate_on_submit():
-        url = request.form['url']
-        cust = request.form['custom']
-        if requests.get(url).status_code != 200:
-            return "your link is not valid"
-        a = Href.query.filter_by(url=url).first()
-
-        if not a:
-            if cust != '':
-                newurl = cust
-            else:
-                newurl = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for x in range(8))
-            href = Href(url, newurl)
-            db.session.add(href)
-            db.session.commit()
-            return my + newurl
-        else:
-            return my + db.session.query(Href.new_url).filter(Href.url == url).scalar()
+        return short_save(request.form['url'], request.form['custom'])
     return render_template('main.html', title='Change URL', form=form)
 
 
 @app.route('/<string:url>', methods=['GET'])
-def redir(url):
-    if not Href.query.filter_by(new_url=url).first():
-        return "No such url"
-    res = db.session.query(Href.url).filter(Href.new_url == url).scalar()
-    return redirect(res)
+def forward(url):
+    return redirect(return_url(url))
